8d4bbabe451a432d15798329ffa0ae1534d1c7cc
Raziel Paulo <rzpaulo@hotmail.com>
2026-02-02 02:08:10 +0000

    a

---
+++ ToniEmprega/Models/HomeDashboardVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ToniEmprega.ViewModels
+{
+    public class HomeDashboardVM
+    {
+        public string Role { get; set; }
+        public int TotalOfertas { get; set; }
+        public int MinhasOfertas { get; set; }
+        public int MinhasCandidaturas { get; set; }
+        public int CandidaturasPendentes { get; set; }
+        public string EstadoValidacao { get; set; }
+    }
+}
---
+++ ToniEmprega/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace ToniEmprega.Models
+{
+    public class HomeViewModel
+    {
+        public string UserName { get; set; }
+        public string Role { get; set; }
+        public SnapshotViewModel Snapshot { get; set; }
+        public List<Offer> Offers { get; set; }
+        public List<Application> Applications { get; set; }
+    }
+
+
+    public class SnapshotViewModel
+    {
+        public int MyApplications { get; set; }
+        public int MatchingOffers { get; set; }
+        public int PendingDocuments { get; set; }
+    }
+
+
+    public class Offer
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public string Summary { get; set; }
+        public bool IsSaved { get; set; }
+        public bool IsPublished { get; set; }
+    }
+
+
+    public class Application
+    {
+        public int Id { get; set; }
+        public int OfferId { get; set; }
+        public string OfferTitle { get; set; }
+        public string Status { get; set; }
+    }
+}
